9e4a07f11f48d4535b53d8560f0c08b393125f9c
Andrew Flierman <andrew@codenamed.nl>
2017-01-06 22:18:58 +0100

    Removed culture specific methods

--- src/resources/value-converters/converters.js
+++ src/resources/value-converters/converters.js
@@ -2,8 +2,6 @@ import numeral from "numeral";
 
 export class CurrencyFormatValueConverter {
     toView(value) {
-        numeral.language("nl-nl", nl);
-        numeral.language("nl-nl");
         return numeral(value).format("$0,0.[00]");
     }
 }
